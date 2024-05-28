b35b4ef758f249f9d27ac54ed25ea0313e7b1275
Radexman <Borderlandsmaniak@gmail.com>
2024-05-28 22:02:23 +0200

    Routes

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,11 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.get('/', (req, res) => {
-	res.status(200).json({ message: 'Welcome to the Support Desk API' });
+	res.status(201).json({ message: 'Hello and welcome to the Support Desk API' });
 });
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
 
 app.listen(PORT, () => {
 	console.log('Server started on port:', PORT);
