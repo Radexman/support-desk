b73f8c0613e3a0b5c28977269bce2745b2c5d855
Radexman <Borderlandsmaniak@gmail.com>
2024-05-30 00:17:57 +0200

    Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 function App() {
 	return (
-		<header>
-			<h1>Hello Redux</h1>
-		</header>
+		<>
+			<Router>
+				<div className='container'>
+					<Header />
+					<Routes>
+						<Route
+							path='/'
+							element={<Home />}
+						/>
+						<Route
+							path='/login'
+							element={<Login />}
+						/>
+						<Route
+							path='/register'
+							element={<Register />}
+						/>
+					</Routes>
+				</div>
+			</Router>
+		</>
 	);
 }
 
