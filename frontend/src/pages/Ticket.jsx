b618e350e38cfda671a602f08d6b1b93dbfab784
Radexman <Borderlandsmaniak@gmail.com>
2024-06-02 15:08:07 +0200

    Close Ticket

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { getTicket } from '../features/Tickets/ticketSlice';
+import { useParams, useNavigate } from 'react-router-dom';
+import { getTicket, closeTicket } from '../features/Tickets/ticketSlice';
 import { toast } from 'react-toastify';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
@@ -10,6 +10,7 @@ const Ticket = () => {
 	const { ticket, isLoading, isError, message } = useSelector((state) => state.tickets);
 
 	const dispatch = useDispatch();
+	const navigate = useNavigate();
 	const { ticketId } = useParams();
 
 	useEffect(() => {
@@ -20,6 +21,13 @@ const Ticket = () => {
 		dispatch(getTicket(ticketId));
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [isError, message, ticketId]);
+
+	// Close ticket
+	const handleTicketClose = () => {
+		dispatch(closeTicket(ticketId));
+		toast.success('Ticket Closed');
+		navigate('/tickets');
+	};
 
 	if (isLoading) {
 		return <Spinner />;
@@ -36,12 +44,21 @@ const Ticket = () => {
 				<h1>Ticket ID: {ticket._id}</h1>
 				<span className={`status status-${ticket.status}`}>{ticket.status}</span>
 				<h2>Date Submitted: {new Date(ticket.createdAt).toLocaleString('en-US')}</h2>
+				<h2>Product: {ticket.product}</h2>
 				<hr />
 				<div className='ticket-desc'>
 					<h3>Description of Issue</h3>
 					<p>{ticket.description}</p>
 				</div>
 			</header>
+			{ticket.status !== 'closed' && (
+				<button
+					onClick={handleTicketClose}
+					className='btn btn-block btn-danger'
+				>
+					Close Ticket
+				</button>
+			)}
 		</div>
 	);
 };
