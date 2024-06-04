c8934ed074074b45d876bfc5915a8b9708e712ea
Radexman <Borderlandsmaniak@gmail.com>
2024-06-04 14:19:09 +0200

    Fetch Notes Redux

--- frontend/store.js
+++ frontend/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './src/features/Auth/authSlice';
 import ticketReducer from './src/features/Tickets/ticketSlice';
+import noteReducer from './src/features/Notes/noteSlice';
 
 const store = configureStore({
 	reducer: {
 		auth: authReducer,
 		tickets: ticketReducer,
+		notes: noteReducer,
 	},
 });
 
